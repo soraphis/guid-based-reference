b053002c7147c65bb9dd40ba7a020f4c43e6267e
Soraphis <soraphis@users.noreply.github.com>
2025-09-22 19:02:49 +0200

    Refactor GuidComponentDrawer to use UIElements
    - guid is now a readonly textfield that is selectable and thus easy to copy

--- Editor/GuidComponentDrawer.cs
+++ Editor/GuidComponentDrawer.cs
@@ -1,27 +1,61 @@
-﻿using UnityEditor;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
 
-namespace UnityEngine.GUID {
-  [CustomEditor(typeof(GuidComponent))]
-  public class GuidComponentDrawer : Editor {
-    private GuidComponent guidComp;
+namespace UnityEngine.GUID
+{
+    [CustomEditor(typeof(GuidComponent))]
+    public class GuidComponentDrawer : Editor
+    {
+        private GuidComponent guidComp;
 
-    public override void OnInspectorGUI () {
-      if (guidComp == null) {
-        guidComp = (GuidComponent) target;
-      }
+        public override VisualElement CreateInspectorGUI()
+        {
+            if (guidComp == null)
+            {
+                guidComp = (GuidComponent)target;
+            }
 
-      // Draw label
-      var guid = guidComp.GetGuid().ToString();
-      using (new EditorGUILayout.HorizontalScope())
-      {
-          EditorGUILayout.PrefixLabel("Guid");
-          EditorGUILayout.SelectableLabel(guid);
-      }
-      if (Event.current.clickCount == 2)
-      {
-          EditorGUIUtility.systemCopyBuffer = guid;
-          Debug.Log("GUID copied: " + guid);
-      }
+            var guid = guidComp.GetGuid().ToString();
+
+            var root = new VisualElement();
+            
+            var row = new PropertyField() { style = { flexDirection = FlexDirection.Row } };
+            row.AddToClassList("unity-property-field");
+            row.AddToClassList("unity-property-field__inspector-property");
+            row.AddToClassList("unity-disabled");
+            
+            var guidField = new TextField { label = "Guid", value = guid, isReadOnly = true };
+            guidField.AddToClassList("unity-base-field__aligned");
+            guidField.AddToClassList("unity-base-field__input");
+            
+            guidField.Q<Label>(className:"unity-label").style.opacity = 1;
+            guidField.Q<Label>(className:"unity-label").RemoveFromClassList("unity-disabled");
+
+            if(TryGetChildTextElement(guidField, out var textElement))
+            {
+                textElement.style.opacity = 0.5f;
+                textElement.AddToClassList("unity-disabled");
+                textElement.AddToClassList("asdfasdf");
+            }
+            row.Add(guidField);
+
+            root.Add(row);
+            return root;
+        }
+
+        private bool TryGetChildTextElement(VisualElement element, out TextElement textElement)
+        {
+            var textInput = element.Q("unity-text-input");
+            if (textInput == null)
+            {
+                Debug.LogError("Could not find TextInput, valid children are: " + string.Join(", ", element.Children().Select(x => x.name)));
+                textElement = null;
+                return false;
+            }
+            textElement = textInput.Q<TextElement>();
+            return textElement != null;
+        }
     }
-  }
 }
